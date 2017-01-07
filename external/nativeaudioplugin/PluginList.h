05be16d370aa376e9f69053aa1db795e0edd7330
Hirotoshi Yoshitaka <tyounan.moti@gmail.com>
2017-01-07 09:14:43 +0900

    プラグインのひな形を作成

---
+++ external/nativeaudioplugin/PluginList.h
@@ -0,0 +1,1 @@
+DECLARE_EFFECT("WASAPI Redirector", WASAPI_redirector)
---
+++ src/entrypoint.cpp
@@ -0,0 +1,20 @@
+#include <IUnityInterface.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+  void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API UnityPluginLoad(IUnityInterfaces* unityInterfaces)
+  {
+
+  }
+
+  void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API UnityPluginUnload()
+  {
+
+  }
+
+#ifdef __cplusplus
+}
+#endif
+
